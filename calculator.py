9a362df7266bc7b76189850d4d20d53ea19f827b
RhettRoseman <rroseman@rcplanners.com>
2024-03-26 14:09:19 -0400

    hello.py lesson portion, calculator.py creation and initial code creation

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# define x define y 
+x = int(input("What's x?"))
+y = int(input("What's y?"))
+
+# function above is the same as the below this one is split between 3 lines
+# x = input("What's x?")
+# y = input("What's y?")
+# integer x + integer y the above does the same thing with less lines
+# z = int(x) + int(y)
+
+# function below is the sum of the original lines 2 and 3
+print(x + y )
--- hello.py
+++ hello.py
@@ -1,4 +1,32 @@
+# str = string
+# Ask user for name 
+# name = input("What's your name? ")
 
-input("What's your name?")
+#do it all with one line
+name = input("What's your name? ").strip().title()
 
-print("hello, world!")
+# Split user's name into first and last name
+first, last = name.split()
+
+# Say Hello to user
+print(f"Hello, {name}!")
+
+
+# Remove whitespace from str
+# name = name.strip().title()
+
+# Capitalize first letter of name
+# name = name.capitalize()
+
+# Title based capitalization
+# name = name.title()
+
+
+
+# print("Hello, " + name + "!"
+# print("Hello, ", name, "!", sep="???")
+
+# print(*objects, sep=' ', end='\n', file=None, flush=False)
+
+
+# print("Hello ,  friend")
