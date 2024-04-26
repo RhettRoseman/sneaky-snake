6ca258167fd94e7f53cee7d4e7ef1dd957525ea7
Rhett Roseman <rroseman@rcplanners.com>
2024-04-26 12:19:49 -0400

    comments on all while loops; creation of for.py in loops folder

--- loops/while.py
+++ loops/while.py
@@ -7,9 +7,12 @@
 # while i does not equal 0 then print meow
 # when i equals 0 then stop the loop ---- this will stop your computer from breaking from the infinite loop
 
+# declar the variables
 i = 3 
+# start the while loop -- i does not equal 0 then print meow
 while i != 0:
     print("meow")
+    # count down from 3 and increment by -1 till we get to 0
     i = i - 1
 
 
---
+++ loops/while1.py
@@ -0,0 +1,11 @@
+# Another way to write a while loop 
+
+# declare the variable i
+i = 1
+# while i is less than or equal to 3 print meow
+while i <= 3:
+    print("meow")
+    # start from 0 and increment by 1 till we get to 3
+    i = i + 1
+
+    # expected export meow meow meow 
