089ee0e9c336c6072d4c3db10377711714c15ca6
Rhett Roseman <rroseman@rcplanners.com>
2024-08-12 13:02:16 -0400

    creation of OOP OOP/getter-setter OOP/int OOP/objects OOP/objects/int-str OOP/objects/initialize OOP/tuple

---
+++ OOP/getter-setter/house1.py
@@ -0,0 +1,55 @@
+class Student:
+    
+    # __init__ intialize an otherwise empty object  (arguments to pass through to constructor)
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Name cannot be empty")
+        # if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]: # list of valid houses, if the user does not enter one of these four houses, errors will be raised
+        #     raise ValueError("Invalid House")
+        self.name = name
+        self.house = house
+        
+    # __str__ method returns a string representation of the object
+    def __str__(self):
+        return f"{self.name} from {self.house}" 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name  # using underscore so computer knows difference between fucntion and property
+    # Getter - gets an attribute
+    @property
+    def house(self):
+        return self._house
+    
+    # Setter- sets an atribute 
+    @house.setter
+    def house(self, house):
+         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]: # list of valid houses, if the user does not enter one of these four houses, errors will be raised
+            raise ValueError("Invalid House")
+           
+         self._house = house # using underscore so computer knows difference between fucntion and property
+        
+# main function prints out the name and house of the student.   
+def main():
+    student = get_student()
+    print(student)
+# get student function prints out the name and house of the student
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house) # <----- constructor call constructs the Student object
+    return student # <----- return the student variable that is the Student object
+
+if __name__ == "__main__":
+    main()
+    
+# expected input:
+# Name: Harry
+# House: Gryffindor
+# expected output:  
+# Harry from Gryffindor
